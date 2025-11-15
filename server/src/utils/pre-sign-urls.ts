aa121e7f145e5fc0852a93d990d0fe7eb4edae80
vitalie <vitalikgriza@gmail.com>
2025-11-15 17:18:00 +0200

    Fix preSignUrl function return

--- server/src/utils/pre-sign-urls.ts
+++ server/src/utils/pre-sign-urls.ts
@@ -2,7 +2,7 @@ import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export const preSignUrls = async (relativeUrls: string[], client: S3Client) => {
-  await Promise.all(
+  return await Promise.all(
     relativeUrls.map(async (relativeUrl) => {
       const command = new GetObjectCommand({
         Bucket: process.env.AWS_S3_BUCKET_NAME!,
