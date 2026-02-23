8f06dc13024490e9ab205d30027a1adaad2fc4d0
Vitalik Griza <vitalikgriza@gmail.com>
2026-02-23 21:05:14 +0000

    Rename Rentiful to Renti in text across client app

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -54,7 +54,7 @@ const NavBar = () => {
             <div className="flex items-center gap-3">
               <Image
                 src="/logo.svg"
-                alt="Rentiful Logo"
+                alt="Renti Logo"
                 width={24}
                 height={24}
                 className="h-6 w-6"
