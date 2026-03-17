0e53df697ef76bcd3e191f567cdfb07700164d01
Vitalik Griza <vitalikgriza@gmail.com>
2026-03-17 19:03:42 +0000

    Rename Rentissss branding to Rentis

--- client/src/app/(nondashboard)/landing/footer-section.tsx
+++ client/src/app/(nondashboard)/landing/footer-section.tsx
@@ -15,7 +15,7 @@ function FooterSection() {
         <div className="flex flex-col md:flex-row items-center justify-between">
           <div className="mb-4">
             <Link href="/" className="text-xl font-bold " scroll={false}>
-              RENTISSSS
+              RENTIS
             </Link>
           </div>
           <nav className="mb-4">
@@ -66,7 +66,7 @@ function FooterSection() {
         </div>
         <div className="mt-8 text-center text-sm text-gray-500 justify-between space-x-6">
           <span>
-            &copy; Rentissss {new Date().getFullYear()}. All rights reserved.
+            &copy; Rentis {new Date().getFullYear()}. All rights reserved.
           </span>
           <Link href="/privacy">Privacy Policy</Link>
           <Link href="/terms">Terms of Service</Link>
